9123d02633ffb8ba1632a5ba6acbc36c83147e25
fsmyth <finn.smyth97@gmail.com>
2020-10-20 22:42:14 +0100

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int moveSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)) {
+        rb.velocity = new Vector2(0, moveSpeed);
+    } else if(Input.GetKey(KeyCode.A)) {
+        rb.velocity = new Vector2(-moveSpeed, 0);
+    }  else if(Input.GetKey(KeyCode.S)) {
+        rb.velocity = new Vector2(0, -moveSpeed);
+    } else if(Input.GetKey(KeyCode.D)) {
+        rb.velocity = new Vector2(moveSpeed, 0);
+    } else rb.velocity = new Vector2(0,0);
+    }
+}
+
